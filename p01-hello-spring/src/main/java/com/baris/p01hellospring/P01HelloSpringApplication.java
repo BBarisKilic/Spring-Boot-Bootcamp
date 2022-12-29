14958ab923c5d5898ff072f86f3f3487608da9f5
Bulent Baris Kilic <baris.kilic@mway.io>
2022-12-29 23:00:01 +0100

    first commit

---
+++ p01-hello-spring/src/main/java/com/baris/p01hellospring/P01HelloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.baris.p01hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P01HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P01HelloSpringApplication.class, args);
+	}
+
+}
---
+++ p01-hello-spring/src/test/java/com/baris/p01hellospring/P01HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baris.p01hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P01HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
