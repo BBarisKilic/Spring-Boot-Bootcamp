82304058f9cd9b3828d2376e4b4b59c60535569f
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-12-30 01:01:31 +0100

    Created painting project.

---
+++ p02-painting/src/main/java/com/baris/p02painting/P02PaintingApplication.java
@@ -0,0 +1,13 @@
+package com.baris.p02painting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P02PaintingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P02PaintingApplication.class, args);
+	}
+
+}
---
+++ p02-painting/src/test/java/com/baris/p02painting/P02PaintingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baris.p02painting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P02PaintingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
