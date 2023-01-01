a296ff97aa13cd849ba8599ed440d2b86ea44497
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-01-01 19:55:28 +0100

    Created show project.

---
+++ p04-show/src/main/java/com/baris/p04show/P04ShowApplication.java
@@ -0,0 +1,13 @@
+package com.baris.p04show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P04ShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P04ShowApplication.class, args);
+	}
+
+}
---
+++ p04-show/src/test/java/com/baris/p04show/P04ShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baris.p04show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P04ShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
